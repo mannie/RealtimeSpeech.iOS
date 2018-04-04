b7156382ef2971361b3b80cace46f60f5e3ef736
Mannie <mail@mannie.io>
2018-04-03 18:26:40 -0700

    Initial Commit

---
+++ RealtimeSpeech.iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealtimeSpeech.iOS
+//
+//  Created by Mannie Tagarira on 4/3/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
